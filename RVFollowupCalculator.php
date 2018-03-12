4bd9e532f30b145ac1dac2e0d6bc871981bbed19
r-cloutier <cloutier@astro.utoronto.ca>
2018-03-11 21:37:59 -0400

    about to update RVFC with the new parameters

--- RVFollowupCalculator.php
+++ RVFollowupCalculator.php
@@ -113,9 +113,35 @@ td, th {
 	    	<FONT COLOR="990000">(optional: select a spectrograph template)&nbsp;&nbsp;&nbsp;</FONT>
             	<select name="spectrograph">
                     <option value="nospec">--</option>
+                    <option value="SpectrographFiles/apfinfile.txt">APF</option>
+                    <option value="SpectrographFiles/apogeeinfile.txt">APOGEE</option>
+                    <option value="SpectrographFiles/carmenesirinfile.txt">CARMENES-IR</option>
+                    <option value="SpectrographFiles/carmenesvisinfile.txt">CARMENES-VIS</option>
+                    <option value="SpectrographFiles/coralieinfile.txt">CORALIE</option>
+                    <option value="SpectrographFiles/espressoinfile.txt">ESPRESSO</option>
+                    <option value="SpectrographFiles/expresinfile.txt">EXPRES</option>
+                    <option value="SpectrographFiles/gclefinfile.txt">G-CLEF</option>
+                    <option value="SpectrographFiles/gianoinfile.txt">GIANO</option>
                     <option value="SpectrographFiles/harpsinfile.txt">HARPS</option>
+                    <option value="SpectrographFiles/harpsninfile.txt">HARPS-N</option>
+                    <option value="SpectrographFiles/hdsinfile.txt">HDS</option>
+                    <option value="SpectrographFiles/hiresinfile.txt">HIRES</option>
+                    <option value="SpectrographFiles/hpfinfile.txt">HPF</option>
+                    <option value="SpectrographFiles/irdinfile.txt">IRD</option>
+                    <option value="SpectrographFiles/ishellinfile.txt">iSHELL</option>
+                    <option value="SpectrographFiles/maroonxinfile.txt">MAROON-X</option>
+                    <option value="SpectrographFiles/minervainfile.txt">MINERVA</option>
+                    <option value="SpectrographFiles/minervaredinfile.txt">MINERVA-Red</option>
+                    <option value="SpectrographFiles/neidinfile.txt">NEID</option>
                     <option value="SpectrographFiles/nirpsinfile.txt">NIRPS</option>
+                    <option value="SpectrographFiles/nresinfile.txt">NRES</option>
+                    <option value="SpectrographFiles/parasinfile.txt">PARAS</option>
+                    <option value="SpectrographFiles/pepsiinfile.txt">PEPSI</option>
+                    <option value="SpectrographFiles/pfsinfile.txt">PFS</option>
+                    <option value="SpectrographFiles/salthrsinfile.txt">SALT-HRS</option>
+                    <option value="SpectrographFiles/sophieinfile.txt">SOPHIE</option>
                     <option value="SpectrographFiles/spirouinfile.txt">SPIRou</option>
+                    <option value="SpectrographFiles/wisdominfile.txt">WISDOM</option>
             	</select>&nbsp;&nbsp;&nbsp;
 	        <input type="submit" name="submit_spec" value="Resolve spectrograph" />
 	    	<br>
@@ -205,9 +231,35 @@ td, th {
                 <FONT COLOR="990000">(optional: select a spectrograph template)&nbsp;&nbsp;&nbsp;</FONT>
                 <select name="spectrograph">
                     <option value="nospec">--</option>
+                    <option value="SpectrographFiles/apfinfile.txt">APF</option>
+                    <option value="SpectrographFiles/apogeeinfile.txt">APOGEE</option>
+                    <option value="SpectrographFiles/carmenesirinfile.txt">CARMENES-IR</option>
+                    <option value="SpectrographFiles/carmenesvisinfile.txt">CARMENES-VIS</option>
+                    <option value="SpectrographFiles/coralieinfile.txt">CORALIE</option>
+                    <option value="SpectrographFiles/espressoinfile.txt">ESPRESSO</option>
+                    <option value="SpectrographFiles/expresinfile.txt">EXPRES</option>
+                    <option value="SpectrographFiles/gclefinfile.txt">G-CLEF</option>
+                    <option value="SpectrographFiles/gianoinfile.txt">GIANO</option>
                     <option value="SpectrographFiles/harpsinfile.txt">HARPS</option>
+                    <option value="SpectrographFiles/harpsninfile.txt">HARPS-N</option>
+                    <option value="SpectrographFiles/hdsinfile.txt">HDS</option>
+                    <option value="SpectrographFiles/hiresinfile.txt">HIRES</option>
+                    <option value="SpectrographFiles/hpfinfile.txt">HPF</option>
+                    <option value="SpectrographFiles/irdinfile.txt">IRD</option>
+                    <option value="SpectrographFiles/ishellinfile.txt">iSHELL</option>
+                    <option value="SpectrographFiles/maroonxinfile.txt">MAROON-X</option>
+                    <option value="SpectrographFiles/minervainfile.txt">MINERVA</option>
+                    <option value="SpectrographFiles/minervaredinfile.txt">MINERVA-Red</option>
+                    <option value="SpectrographFiles/neidinfile.txt">NEID</option>
                     <option value="SpectrographFiles/nirpsinfile.txt">NIRPS</option>
+                    <option value="SpectrographFiles/nresinfile.txt">NRES</option>
+                    <option value="SpectrographFiles/parasinfile.txt">PARAS</option>
+                    <option value="SpectrographFiles/pepsiinfile.txt">PEPSI</option>
+                    <option value="SpectrographFiles/pfsinfile.txt">PFS</option>
+                    <option value="SpectrographFiles/salthrsinfile.txt">SALT-HRS</option>
+                    <option value="SpectrographFiles/sophieinfile.txt">SOPHIE</option>
                     <option value="SpectrographFiles/spirouinfile.txt">SPIRou</option>
+                    <option value="SpectrographFiles/wisdominfile.txt">WISDOM</option>
                 </select>&nbsp;&nbsp;&nbsp;
                 <input type="submit" name="submit_spec" value="Resolve spectrograph" />
                 <br>
@@ -405,7 +457,7 @@ td, th {
                         <td style="padding: 0px 0px 10px 30px;">Desired K detection signficance (K/&sigma;<sub>K</sub>) :&nbsp;&nbsp;<input type="text" name="Kdetsig" value="<?php echo isset($_GET['Kdetsig']) ? $_GET['Kdetsig'] : 3 ?>"  size="10" maxlength="50"/></td>
                     </tr>
                     <tr>
-                        <td style="padding: 0px 0px 10px 30px;">Number of trials :&nbsp;&nbsp;<input type="text" name="Ntrials" value="<?php echo isset($_GET['Ntrials']) ? $_GET['Ntrials'] : 100 ?>"  size="10" maxlength="50"/></td>
+                        <td style="padding: 0px 0px 10px 30px;">Number of trials :&nbsp;&nbsp;<input type="text" name="Ntrials" value="<?php echo isset($_GET['Ntrials']) ? $_GET['Ntrials'] : 1 ?>"  size="10" maxlength="50"/></td>
                     </tr>
                 </table>
                 &emsp;&emsp;<input type="checkbox" value="runGP" <?php if (!isset($_GET['runGP'])) : ?> checked<?php endif; ?> name="runGP">&nbsp;&nbsp;check to compute the total observing time including a quasi-periodic Gaussian process activity model (will significantly slow the calculation wall time)
